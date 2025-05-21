23b6cb4df5065357b58d0a140556925e475a6910
Divyendu Singh <mail@divyendusingh.com>
2025-05-21 09:16:09 +0200

    chore: remove bundle analyzer

--- packages/next-js-app/next.config.js
+++ packages/next-js-app/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 const config = {
   reactStrictMode: true,
@@ -18,4 +15,4 @@ const config = {
   transpilePackages: ['@trackfootball/*'],
 }
 
-module.exports = withBundleAnalyzer(config)
+module.exports = config
